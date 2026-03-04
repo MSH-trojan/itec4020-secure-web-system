3d9b70b0b557bde6c9d168af8b0d0f5b50d6dcd6
MSH <157653802+MSH-trojan@users.noreply.github.com>
2026-03-04 13:52:16 -0500

    Delete Website/website_sit/website directory

--- Website/website_sit/website/js/disableSelection.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("selectstart", function (e) {
-  e.preventDefault();
-});
-
-document.addEventListener("keydown", function (e) {
-  if (e.ctrlKey && (e.key === "c" || e.key === "a")) {
-    e.preventDefault();
-  }
-});
--- Website/website_sit/website/src/TestRes.java
+++
@@ -1,4 +0,0 @@
-import javax.servlet.ServletContext;
-public class TestRes {
-  public static void main(String[] args) {}
-}
